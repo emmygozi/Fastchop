af10b2888e255888ddd3e917bf582c2f0f7160d0
emmygozi <mygitmail@yahoo.com>
2018-09-12 13:55:40 +0100

    [Feature #160447709] Test get all food items api endpoint

---
+++ server/tests/foodItem/getAll.test.js
@@ -0,0 +1,20 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('GET /', () => {
+  it('should return a success status 200', async () => {
+    try {
+      const res = await chai.request(app)
+        .get('/api/v1/foodItem');
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('array');
+    } catch (err) {
+      throw err.message;
+    }
+  });
+});
+
